abe73afced95aa7bfeee756c98ee9851bc76ee1f
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-01-02 15:20:23 +0600

    fixed the commit issue by @asif17r

--- src/tryout/Student.java
+++ src/tryout/Student.java
@@ -38,16 +38,18 @@ public class Student {
     }
 
     public static void main(String[] args) throws SQLException {
-        String sql = "SELECT name FROM Student";
-        // Add from constants
-        String url = Constants.DB_URL;
-        String user = Constants.DB_USER;
-        String password = Constants.DB_PASSWORD;
-        Connection connection = DriverManager.getConnection(url, user, password);
-        Statement statement = connection.createStatement();
-        statement.executeQuery(sql);
-        ResultSet rs = statement.executeQuery(sql);
-        rs.next();
-        System.out.println(rs.getString("name"));
+        initializeDatabase();
+//        fillDummy();
+        String sql = "SELECT name FROM " + Constants.TABLE_NAME; // Use double quotes to preserve case
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+        }
+
+        //call create to add one more entry
+        CRUD.createStudent("AsifProPlus", "Universe", 4.1);
     }
 }
