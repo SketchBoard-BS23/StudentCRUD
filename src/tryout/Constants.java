c6ecb557bc3e3f23507f7b852a47906fe182d0a9
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-01-02 10:34:55 +0600

    Added seperate constant class for strings, this is cheap replacement of .env file. Avoiding .env to keep things simple. This Fixes #8.

---
+++ src/tryout/Constants.java
@@ -0,0 +1,14 @@
+package tryout;
+
+public final class Constants {
+    private Constants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    // Define string constants
+    public static final String DB_NAME = "student_crud";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "0000";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/" + DB_NAME;
+
+}
--- src/tryout/Student.java
+++ src/tryout/Student.java
@@ -1,13 +1,15 @@
 package tryout;
 
 import java.sql.*;
+import tryout.Constants;
 
 public class Student {
     public static void main(String[] args) throws SQLException {
         String sql = "SELECT name FROM Student";
-        String url = "jdbc:postgresql://localhost:5432/student_crud";
-        String user = "postgres";
-        String password = "0000";
+        // Add from constants
+        String url = Constants.DB_URL;
+        String user = Constants.DB_USER;
+        String password = Constants.DB_PASSWORD;
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
         statement.executeQuery(sql);
